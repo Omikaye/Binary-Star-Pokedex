19398780e59631b23e33adf79398760766362163
Omikaye <omi.phifer@gmail.com>
2025-11-26 00:34:12 -0800

    Update dist.ts

--- build/dist.ts
+++ build/dist.ts
@@ -8,9 +8,10 @@ let dest = argv[2];
 rmSync(`./${dest}`, {recursive: true, force: true})
 mkdirSync(`./${dest}`)
 
+execSync(`npx parcel build --public-url ${config.baseurl} ./index.html`)
+
 for (let folder of ["images"]) {
   cpSync(`${folder}`, `./${dest}/`, {recursive: true})
 }
 
-execSync(`npx parcel build --public-url ${config.baseurl} ./index.html`)
 cpSync(`./${dest}/index.html`, `./${dest}/404.html`)
